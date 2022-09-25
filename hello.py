af0478b36a09cb4ce51283b4179d973dde9003fe
EmmaZhao0413 <emmazhao@LAPTOP-HDJSDEGP.localdomain>
2022-09-25 17:46:30 -0400

    Replay and modify example in Chapter 4

--- hello.py
+++ hello.py
@@ -1,18 +1,49 @@
-from flask import Flask,render_template
+from flask import Flask,render_template,session,redirect,url_for,flash
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
 from datetime import datetime
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField, EmailField
+from wtforms.validators import DataRequired,Email
 
 app=Flask(__name__)
 bootstrap=Bootstrap(app)
 moment=Moment(app)
+app.config['SECRET_KEY']='hard to guess string'
 
+class NameForm(FlaskForm):
+	name=StringField('What is your name?', validators=[DataRequired()])
+	email=EmailField('What is your UofT Email address?', validators=[DataRequired(),Email()])
+	submit=SubmitField('Submit')
 
-@app.route('/')
+@app.route('/',methods=['GET','POST'])
 def index():
-	return render_template('index.html', current_time=datetime.utcnow())
+	isEmail=None
+	form=NameForm()
+	old_name=session.get('name')
+	if form.validate_on_submit():
+		old_name=session.get("name")
+		old_email=session.get("email")
+		if old_name is not None and old_name!=form.name.data:
+			flash('Looks like you have changed your name!')
+		if old_email is not None and old_email!=form.email.data:
+			flash('Looks like you have changed your email')
+		if 'utoronto' not in form.email.data:
+			isEmail=False
+		else:
+			isEmail=True
+		session['name']=form.name.data
+		session['email']=form.email.data
+		session['isEmail']=isEmail
+		form.name.data=""
+		form.name.data=""
+		isEmail=None
+		return redirect(url_for('index'))
+	return render_template('index.html', form=form, name=session.get('name'), email=session.get('email'),isEmail=session.get('isEmail'))
 app.add_url_rule('/','index', index)
 @app.route('/user/<name>')
 def user(name):
 	return render_template('user.html', name=name, current_time=datetime.utcnow())
 
+if __name__ == '__main__':
+	app.run(debug=True)
